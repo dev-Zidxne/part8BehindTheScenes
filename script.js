805224e5f70e88bdecad8292919342ea1f5027c7
Zidane Innis <zidaneinnis@gmail.com>
2022-04-30 13:09:35 -0500

    Hoisting and Temporal Dead Zone(TDZ) in practice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   console.log(firstName);
@@ -33,5 +33,58 @@ function calcAge(birthYear) {
 const firstName = 'Zidane';
 calcAge(1991);
 
+
 // console.log(age);
 // printAge();
+*/
+
+// Hoisting and TDZ in practice
+
+// Variables
+console.log(me);
+
+// console.log(job);
+
+// console.log(year);
+
+var me = 'Zidane';
+let job = 'developer';
+const year = '2001';
+
+// Functions
+
+console.log(addDecl(2, 3));
+
+// console.log(addExpr(2, 3));
+
+console.log(addArrow);
+
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.x);
+console.log(z === window.x);
